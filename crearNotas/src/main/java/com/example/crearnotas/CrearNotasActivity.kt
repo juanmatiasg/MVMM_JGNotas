1821162c2fd90d009169d15404709853b8f7a324
Juan Guerrero <juanmatiasg@hotmail.com>
2020-10-15 18:52:45 -0300

    Primer Proyecto MVMMNotas

---
+++ crearNotas/src/main/java/com/example/crearnotas/CrearNotasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.crearnotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearNotasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_notas)
+    }
+}
